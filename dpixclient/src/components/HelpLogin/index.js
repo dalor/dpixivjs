0f81254c13a82504b999dcc65b2d91f8d1e4ad64
DALOR <dalor@i.ua>
2021-04-09 23:19:13 +0300

    Update index.js

--- dpixclient/src/components/HelpLogin/index.js
+++ dpixclient/src/components/HelpLogin/index.js
@@ -8,13 +8,13 @@ export default () => {
 
     const [copied, setCopied] = useState(false)
 
-    const code = `document.body.appendChild(Object.assign(document.createElement("script"),{crossorigin:"Anonymous",defer:true,src:"${window.location.origin}/script.js"}))`;
+    const code = `document.body.appendChild(Object.assign(document.createElement("script"),{crossorigin:"Anonymous",defer:true,src:"https://${window.location.hostname}/script.js"}))`;
 
     return (
         <Wrapper width="80%" big={true}>
             <div className="help-login">
                 <h2 className="center">How to login guide</h2>
-                <img src={LoginScreen} alt="Login screen"/>
+                <img src={LoginScreen} alt="Login screen" />
                 <p>
                     Go to page <a href="https://accounts.pixiv.net/login">accounts.pixiv.net/login</a> and enter code:
                 </p>
